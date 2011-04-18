e4fd928c24de603cd0a866e3a9298fba1a7ee068
Calli Higgins <calli@nyu.edu>
2011-04-18 16:50:11 -0400

    heroku fixes

--- models.rb
+++ models.rb
@@ -18,7 +18,7 @@ class Capsule
   property :path,       String    
   #property :body,       Text      
   property :created_at, DateTime
-  property :sent,		Boolean, :default => false
+ # property :sent,		Boolean, :default => false
   
   
   #TODO:
@@ -29,20 +29,20 @@ class Capsule
   #-> in the Rakefile task call Capsule.send_due_capsules!
   #-> add the cron addon to your app on heroku
   
-  def self.send_due_capsules!
+ # def self.send_due_capsules!
   	# get all due_capsules
   	# for each due_capsule
   	# tell the capsule to send
   	# set the sent property to true
   	# and save the capsule 
-  end
+#  end
   
   
-  def self.due_capsules
+#  def self.due_capsules
   	# get all the capsules whose dueDate is less than the current DateTime
   #	Capsule.all :dueDate.lt => DateTime.now :sent == false
   	# and whose sent flag is not true
-  end
+ # end
   
   def path_string(filename)
   #"foo.jpg" "foo.tiff"
