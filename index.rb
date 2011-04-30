66dd570b753b08e53e9a4efd38fb83cac19b9e7c
Calli Higgins <calli@nyu.edu>
2011-04-30 16:53:43 -0400

    added sync profiles for orientation, maybe

--- index.rb
+++ index.rb
@@ -50,7 +50,7 @@ params[:file] = params[:file].unpack("m")[0]
   
     #filename = params[:file][:filename] # CHANGEME: generate one based on the Capsule id
    # file = params[:file][:tempfile]
-    
+    image.sync_profiles
 # generate a random time
 t = Time.now
 currentyear = t.year
