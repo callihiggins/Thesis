f638e7aa81805ba9cd91df0cd1b334b330c9c770
Calli Higgins <calli@nyu.edu>
2011-07-03 18:03:07 -0400

    removing all timezone changes

--- index.rb
+++ index.rb
@@ -62,9 +62,8 @@ params[:file] = params[:file].unpack("m")[0]
    if params[:file]
    image = Magick::Image.from_blob(params[:file]).first
    image.auto_orient!
-    #image.sync_profiles
 # generate a random time
-t = Time.now("+03:00")
+t = Time.now
 currentyear = t.year
 year = currentyear + rand(6)
 month = rand(11)
