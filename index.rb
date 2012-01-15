b8e7f0e14604225f64ca7599e4125c2aed37f426
callihiggins <calli@nyu.edu>
2012-01-15 18:46:03 -0500

    revertin back after app crash

--- index.rb
+++ index.rb
@@ -31,9 +31,15 @@ get "/capsules/new" do
 erb :new_capsule
 end
 
-get "/capsules/:image_token" do
+#get "/capsules/:image_token" do
 # show the image plus some info about it
-@capsule = Capsule.first :image_token => params[:image_token]
+#@capsule = Capsule.first :image_token => params[:image_token]
+#erb :capsule
+#end
+
+get "/capsules/:id" do
+# show the image plus some info about it
+@capsule = Capsule.get params[:id]
 erb :capsule
 end
 
