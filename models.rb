70c52c0d753789976ef3c1aec3b881f1fbacec3d
callihiggins <calli@nyu.edu>
2012-02-17 17:04:31 -0500

    g

--- models.rb
+++ models.rb
@@ -126,7 +126,7 @@ class Capsule
 	unsent_caps
    end
   
-    def self.send_tag_request!  
+    def send_tag_request!  
     owner = self.user.email
   	 users = self.tagged_users
   	 users.each do |user|
