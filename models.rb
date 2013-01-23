2d7687f3d07caee9470666540ef2b22294760aa5
callihiggins <calli@nyu.edu>
2013-01-23 14:08:33 -0500

    g

--- models.rb
+++ models.rb
@@ -79,12 +79,12 @@ class Capsule
   	# get the image path
   					
   	  				image_path = due_capsule.image_token
-  	  				owner = due_capsule.user.email
+  	  				owner = due_capsule.user
   	  				  	#go through each one and send it to the user
   					due_capsule.taggings.each do |tag|
   						# tell the capsule to send
   						
-  						EmailSender.send(:address => tag.user.email, :subject => "Here's your Throwback!", :body => "You've received a Throwback from " + owner +". Click the link below to view your photo.
+  						EmailSender.send(:address => tag.user.email, :subject => "Here's your Throwback!", :body => "You've received a Throwback from #{owner.email}. Click the link below to view your photo.
   						
   http://throwback-app.com/capsules/" + image_path)
   						# set the tag flag to true
