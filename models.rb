eecea700ea259c1015232fc5b860eb9286d3ad46
callihiggins <calli@nyu.edu>
2013-01-24 14:30:41 -0500

    g

--- models.rb
+++ models.rb
@@ -84,7 +84,7 @@ class Capsule
   					due_capsule.taggings.each do |tag|
   						# tell the capsule to send
   						
-  						EmailSender.send(:address => tag.user.email, :subject => "Here's your Throwback!", :body => "You've received a Throwback from #{owner.email}. Click the link below to view your photo.
+  						EmailSender.send(:address => self.user.email, :subject => "Here's your Throwback!", :body => "You've received a Throwback from #{owner.email}. Click the link below to view your photo.
   						
   http://throwback-app.com/capsules/" + image_path)
   						# set the tag flag to true
