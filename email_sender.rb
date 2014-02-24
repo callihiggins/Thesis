7e7e125cc688726c0e9f4f467ae7e5083bd47606
callihiggins <calli@nyu.edu>
2014-02-23 23:15:48 -0500

    g

--- email_sender.rb
+++ email_sender.rb
@@ -6,7 +6,7 @@ class EmailSender
             :from => 'Throwback@throwback-app.com',
             :subject => params[:subject],
             :body => params[:body], 
-            :html_body => erb(hello_email.erb),
+            :html_body => erb :hello_email,
             :via => :smtp,
             :via_options => { 
                 :address   => 'smtp.sendgrid.net', 
