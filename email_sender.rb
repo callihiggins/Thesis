01b1e6fb8bf6d3e90823cbc64c27e6a1fe074ca0
callihiggins <calli@nyu.edu>
2014-02-24 00:11:38 -0500

    gg

--- email_sender.rb
+++ email_sender.rb
@@ -5,9 +5,10 @@ require 'erb'
 
 
 class EmailSender
-body = ERB.new(File.read('../views/hello_email.erb'))
-body.result(binding)
+
   def self.send(params)
+  body = ERB.new(File.read('./views/hello_email.erb'))
+	body.result(binding)
       Pony.mail :to => params[:address],
             :from => 'Throwback@throwback-app.com',
             :subject => params[:subject],
