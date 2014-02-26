7fb535b11bc7731ece7a6e12e3f3c894f6f566c3
callihiggins <calli@nyu.edu>
2014-02-25 22:16:11 -0500

    g

--- models.rb
+++ models.rb
@@ -53,8 +53,7 @@ http://throwback-app.com/users/reset/#{self.user_token}")
   	   def send_hello_email! 
   	   
   	      body = ERB.new(File.read('./views/hello_email.erb'))
-   	      body.result(binding)
-
+   	     
   	   
   	EmailSender.send(:address => self.email, :subject => "Welcome to Throwback", :body=>body) 
   	
